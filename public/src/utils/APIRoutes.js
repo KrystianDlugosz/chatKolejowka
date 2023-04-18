05cd22b8fa5723ddee355d682e0f932216865e67
krystian <krystianxz15575@gmail.com>
2023-04-18 09:59:44 +0200

    heorku

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,6 +1,6 @@
-export const host = "http://localhost:3000";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
+export const host = "https://chat4k.herokuapp.com";
+export const registerRoute = `${host}/register`;
+export const loginRoute = `${host}/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
